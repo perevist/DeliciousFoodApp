146e430317a246d7ccfe26241b358fa8974ac155
Patryk Fidrych <perevistp@gmail.com>
2021-02-25 23:32:49 +0100

    Initial commit

---
+++ src/main/java/com/perevist/DeliciousFoodApp/DeliciousFoodAppApplication.java
@@ -0,0 +1,12 @@
+package com.perevist.DeliciousFoodApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciousFoodAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliciousFoodAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/perevist/DeliciousFoodApp/DeliciousFoodAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perevist.DeliciousFoodApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciousFoodAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
